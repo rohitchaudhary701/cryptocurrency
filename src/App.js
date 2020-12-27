038f3f632d0226422df9eacef8d5df4eeb92f8ad
Rohit Kumar <rohitku@abpnews.in>
2020-12-27 20:52:53 +0530

    cryptocurrency app

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Coin from './Coin';
 import './App.css';
 
 
-const apiUrl = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
+const apiUrl = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=30&page=1&sparkline=false';
 
 function App() {
 
@@ -14,7 +14,7 @@ function App() {
     axios.get(apiUrl)
     .then(res => {
       setCoins(res.data)
-      console.log(res.data);
+      //console.log(res.data);
     })
     .catch(error => console.log(error));
   }, [])
